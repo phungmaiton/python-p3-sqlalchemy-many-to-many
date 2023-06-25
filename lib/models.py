352220b7ead7d9245b75a9e4bd8e0ed97db57565
Mai <garcia.ton94@gmail.com>
2023-06-25 18:50:03 -0400

    complete lab

--- lib/models.py
+++ lib/models.py
@@ -10,8 +10,17 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+game_user = Table(
+    "game_users",
+    Base.metadata,
+    Column("game_id", ForeignKey("games.id"), primary_key=True),
+    Column("user_id", ForeignKey("users.id"), primary_key=True),
+    extend_existing=True,
+)
+
+
 class Game(Base):
-    __tablename__ = 'games'
+    __tablename__ = "games"
 
     id = Column(Integer(), primary_key=True)
     title = Column(String())
@@ -19,23 +28,59 @@ class Game(Base):
     platform = Column(String())
     price = Column(Integer())
 
-    reviews = relationship('Review', backref=backref('game'))
+    reviews = relationship("Review", backref=backref("game"))
+    users = relationship("User", secondary=game_user, back_populates="games")
 
     def __repr__(self):
-        return f'Game(id={self.id}, ' + \
-            f'title={self.title}, ' + \
-            f'platform={self.platform})'
+        return (
+            f"Game(id={self.id}, "
+            + f"title={self.title}, "
+            + f"platform={self.platform})"
+        )
+
 
 class Review(Base):
-    __tablename__ = 'reviews'
+    __tablename__ = "reviews"
 
     id = Column(Integer(), primary_key=True)
     score = Column(Integer())
     comment = Column(String())
-    
-    game_id = Column(Integer(), ForeignKey('games.id'))
+
+    game_id = Column(Integer(), ForeignKey("games.id"))
+    user_id = Column(Integer(), ForeignKey("users.id"))
 
     def __repr__(self):
-        return f'Review(id={self.id}, ' + \
-            f'score={self.score}, ' + \
-            f'game_id={self.game_id})'
+        return (
+            f"Review(id={self.id}, "
+            + f"score={self.score}, "
+            + f"game_id={self.game_id})"
+        )
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    reviews = relationship("Review", backref=backref("user"))
+    games = relationship("Game", secondary=game_user, back_populates="users")
+
+    def __repr__(self):
+        return f"User(id={self.id}, " + f"name={self.name})"
+
+
+# class GameUser(Base):
+#     __tablename__ = "game_users"
+
+#     id = Column(Integer(), primary_key=True)
+#     game_id = Column(ForeignKey("games.id"))
+#     user_id = Column(ForeignKey("user.id"))
+
+#     game = relationship("Game", back_populates="game_users")
+#     user = relationship("User", back_populates="game_users")
+
+#     def __repr__(self):
+#         return f"GameUser(game_id={self.game_id}) " + f"user_id={self.user_id}"
